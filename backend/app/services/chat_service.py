cdd189452633cf5e3b30f0f4b2d6a0bd56db9c0e
otrahigus <mdasugiharto@gmail.com>
2026-06-03 10:08:22 +0700

    Move SOP knowledge to JSON file

--- backend/app/services/chat_service.py
+++ backend/app/services/chat_service.py
@@ -1,40 +1,14 @@
+import json
+
+with open("data/sop.json", "r", encoding="utf-8") as f:
+    SOP_DATA = json.load(f)
+
 def get_response(text):
 
     text = text.lower()
 
-    if "greeting" in text:
-        return  """
-SOP pelayanan:
-1. Menerima pesanan
-2. Input pesanan
-3. Pembayaran
-4. Pembuatan produk
-5. Penyerahan produk
-6. Membersihkan alat
-"""
-            
-    elif "bonus" in text:
-        return """
-Bonus berdasarkan omset outlet:
-50–59 juta → 0.5%
-60–69 juta → 1%
-70+ juta → 2%
-"""
+    for keyword, response in SOP_DATA.items():
+        if keyword in text:
+            return response
 
-
-    elif "terlambat" in text:
-        return """
-Keterlambatan:
-- Mengurangi 1/3 bonus
-- Berulang bisa kena PHK
-"""
-
-    elif "seragam" in text:
-        return """
-Senin-Selasa: bebas berkerah
-Rabu-Kamis: seragam Mbok Darmi
-Jumat: batik
-Sabtu-Minggu: seragam
-"""
-
-    return "Maaf, SOP belum tersedia."
+    return "Maaf, informasi belum tersedia."
